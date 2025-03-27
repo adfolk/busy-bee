2fe8afa3f9d6dd3d4f5eae150d3c68332c931652
adfolk <adfolkestad@gmail.com>
2025-03-27 18:54:14 +0400

    dummy src files to test parsing on

---
+++ src/simulated_src_code_files/lmao.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/* This is a multiline comment.
+This is a second line.
+TODO: this
+WARNING: Hey it worked!
+*/
+
+func printLmao() {
+	fmt.Println("lmao this is go code")
+}
