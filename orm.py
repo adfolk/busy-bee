01311fe8068f070ab9595e35ee5b8831396347a0
adfolk <adfolkestad@gmail.com>
2025-09-29 21:05:21 +0400

    WIP

--- orm.py
+++ orm.py
@@ -1,24 +1,56 @@
 from database import create_app_db
+from project import Project
 from peewee import CharField, IntegerField, Model, ForeignKeyField, TextField
 
-db = create_app_db()
+database = create_app_db()
+
+def get_project_files_and_create(path_name: str):
+    db = database
+    db.connect()
+    db.create_tables([ProjectRepo, SourceCodeFile, CodeTag])
+
+    proj = Project(path_name)
+    src_files = proj.src_files
+    tagged_files = proj.tagged_src_files
+    proj_orm = ProjectRepo.create(
+        commit_id = proj.commit_id,
+        name = proj.name,
+        path = proj.path
+    )
+    for file in src_files:
+        src_f_orm = SourceCodeFile.create(
+            commit_id = proj.commit_id,
+            blob_id = file.blob,
+            name = file.file_name
+        )
+    for file in tagged_files:
+        for tag in file.tags:
+            tag_orm = CodeTag.create(
+                commit_id = proj.commit_id,
+                parent_blob_id = tag.blob_parent,
+                tag_name = tag.tag_name,
+                message = tag.message,
+                line_num = tag.line_number
+            )
+
+    db.close()
 
 class BaseModel(Model):
     class Meta:
-        database = db
+        database = database
 
-class Project(BaseModel):
+class ProjectRepo(BaseModel):
     commit_id = CharField(max_length=40)
     name = TextField()
     path = TextField()
 
 class SourceCodeFile(BaseModel):
-    commit_id = ForeignKeyField(Project, backref='commit_id')
+    commit_id = ForeignKeyField(ProjectRepo, backref='commit_id')
     blob_id = CharField(max_length=40)
     name = TextField()
 
 class CodeTag(BaseModel):
-    commit_id = ForeignKeyField(Project, backref='commit_id')
+    commit_id = ForeignKeyField(ProjectRepo, backref='commit_id')
     parent_blob_id = ForeignKeyField(SourceCodeFile, backref='blob_id')
     tag_name = TextField()
     message = TextField()
--- test_project.py
+++ test_project.py
@@ -1,28 +1,24 @@
 from project import Project
 import unittest
 
-python_file = '/Users/austinfolkestad/workspace/github.com/adfolk/personal-projects/busy-bee/src/simulated_src_code_files/character.py'
+test_repo_path: str = "/home/adfolk/workspace/github.com/adfolk/experiments/monke/"
 
-adventurer_todo = "Line: 1, Tag: TODO, Message: Write subclass of Adventurer to represent non-wildcard enemies."
-bug_fix = "Line: 37, Tag: FIX, Message: this is a bug"
-compound_todo = "Line: 54, Tag: TODO, Message: write methods to calculate compound attributes."
-problem_fix = "Line: 58, Tag: FIX, Message: problem, officer?"
-
-test_repo_path: str = "/Users/austinfolkestad/workspace/github.com/adfolk/experiments/git-monkey/"
+proj = Project(test_repo_path)
 
 class TestProject(unittest.TestCase):
     def test_project_integration(self):
-        proj = Project(test_repo_path)
         tagged_files = proj.tagged_src_files
         for file in tagged_files:
             for tag in file.tags:
                 print(tag.message)
 
     def test_name(self):
-        proj = Project(test_repo_path)
         proj_name = proj.name
-        self.assertEqual(proj_name, "git-monkey")
+        self.assertEqual(proj_name, "monke")
 
     def test_commit_id_length(self):
-        proj = Project(test_repo_path)
         self.assertEqual(len(proj.commit_id), 40)
+
+    def test_proj_has_files(self):
+        tag_files = proj.tagged_src_files
+        print(tag_files)
