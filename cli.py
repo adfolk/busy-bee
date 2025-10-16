1663fe641b2e472fd47c1fa83f15a8108f6910fe
adfolk <adfolkestad@gmail.com>
2025-10-16 23:06:56 +0400

    started working on flexible flags

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import os
 import typer
+from code_tag import CodeTagEnum
 from database import db, SourceCodeFile, fill_app_tables, CodeTag
 from rich.table import Table
 from rich.console import Console
@@ -8,16 +9,38 @@ from typing_extensions import Annotated
 app = typer.Typer(no_args_is_help=True)
 
 @app.command()
-def Get_Tags(
+def Gather(
     project_path: Annotated[str, typer.Argument()] = os.getcwd(),
-    all_tag_types: Annotated[bool, typer.Option("--all", "-a", help="By default, only TODOs are show. Use this flag to show all tag types")] = False
+    todos: Annotated[bool, typer.Option("--todos", "-t")] = False,
+    bugs: Annotated[bool, typer.Option("--bugs", "-b")] = False,
+    perfs: Annotated[bool, typer.Option("--perfs", "-p")] = False,
+    warns: Annotated[bool, typer.Option("--warns", "-w")] = False,
+    tests: Annotated[bool, typer.Option("--tests", "-t")] = False,
+    all_tags: Annotated[bool, typer.Option("--all", "-a", help="By default, only TODOs are shown. Use this flag to show all tag types")] = False
     ):
     created_project = fill_app_tables(project_path)
     table = make_display_table(created_project.name)
     row_num = 0
 
-    # TODO: figure out how to make this neater.
-    if all_tag_types == False:
+    tag_flag_args: list[str] = []
+
+    if todos:
+        # Yeah, it's weird to have a "--todos" flag default to False when default behavior is to only show todos.
+        tag_flag_args.append(CodeTagEnum.TODO.name)
+    if bugs:
+        tag_flag_args.append(CodeTagEnum.BUG.name)
+    if perfs:
+        tag_flag_args.append(CodeTagEnum.PERF.name)
+    if warns:
+        tag_flag_args.append(CodeTagEnum.WARN.name)
+    if tests:
+        tag_flag_args.append(CodeTagEnum.TEST.name)
+    if all_tags:
+        tag_flag_args = [CodeTagEnum.TODO.name, CodeTagEnum.BUG.name, CodeTagEnum.PERF.name, CodeTagEnum.WARN.name, CodeTagEnum.TEST.name]
+    else:
+        tag_flag_args = [CodeTagEnum.TODO.name]
+
+    if all_tags == False:
         for tag in CodeTag.select().where(CodeTag.commit_id == created_project.commit_id, CodeTag.tag_name == "TODO"):
             file_name = SourceCodeFile.get(SourceCodeFile.blob_id == tag.parent_blob_id).name
             table.add_row(str(row_num), file_name, str(tag.line_num), tag.tag_name, tag.message, tag.commit_id, tag.msg_uid)
